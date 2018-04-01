f77d2ee9fe6a1c23035daeff1d7c26c2d3b57fb0
Michael Morello <michael.morello@gmail.com>
2018-04-01 16:41:41 +0200

    first commit

---
+++ postgresql/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+tag=`git describe --exact-match --tags $(git log -n1 --pretty='%h')`
+regex='([0-9]+)r.*'
+
+if [[ $tag =~ $regex ]]; then
+  #psqlv=${BASH_REMATCH[1]}
+  #echo "Postgresql version is $psqlv"
+  docker build -f Dockerfile -t d1g1tal/postgresql-centos7:${tag} .
+else
+  echo "Cannot decode current git tag"
+  exit 2
+fi
+
+
